e533cc78294e28c07ef7d06034786f6344289b9d
radhikaakhandelwal <k.radhikakhandelwal@gmail.com>
2025-04-28 16:06:01 -0700

    server.js path fix

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ app.use("/api/products", ProductRoutes);
 // if we have deployed this application
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.res(__dirname, "frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
